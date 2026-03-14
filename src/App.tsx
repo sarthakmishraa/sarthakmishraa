7d42e1a3f7e36402538949a880a20e89b3eb752d
Sarthak Mishra <msarthak785@gmail.com>
2026-03-14 23:30:31 +0530

    basename fix in browser router

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { Home } from "./pages/Home";
 function App() {
   return (
     <div className="min-h-screen w-full bg-stone-900 text-gray-300 px-8 sm:px-16 md:px-48 py-12">
-      <Router>
+      <Router basename="/sarthakmishraa">
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
